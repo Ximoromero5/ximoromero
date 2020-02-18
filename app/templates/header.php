20ba968f2e4580093ada19b1ec8611ad79802197
Ximo Romero <chimillo_95@hotmail.com>
2020-02-18 12:09:44 +0100

    login

---
+++ app/templates/header.php
@@ -0,0 +1,27 @@
+ <!-- MENU -->
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+     <a class="navbar-brand" href="#">Ximoromero</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav mr-auto">
+             <li class="nav-item active">
+                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+             </li>
+             <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Dropdown
+                 </a>
+                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                     <a class="dropdown-item" href="#">Profile <i class="fas fa-user ml-2"></i></a>
+                     <a class="dropdown-item" href="#">Settings<i class="fas fa-cog ml-2"></i></a>
+                     <div class="dropdown-divider"></div>
+                     <a class="dropdown-item btn btn-danger" href="#">Log Out <i class="fas fa-sign-out-alt ml-2"></i></a>
+                 </div>
+             </li>
+         </ul>
+     </div>
+ </nav>
+ <!-- END MENU -->
